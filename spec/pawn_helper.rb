f982780275f4651150e07a07d8e171fdebf277c6
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com>
2025-08-28 20:37:41 +0700

    Adds spec file for the new pawn.rb

--- spec/pawn_helper.rb
+++ spec/pawn_helper.rb
@@ -3,134 +3,191 @@ require '../lib/board.rb'
 
 describe Pawn do
   describe '#valid_moves' do
-    context 'when pawn color is white' do
+    context 'when pawn color is white and at default position' do
       let(:board) { Board.new }
-      let(:position) { position = [6, 1] } # second bottom row
+      let(:position) { [6, 1] } # second bottom row
       subject(:pawn) { described_class.new(:white, position) }
+
+      before do
+        row, col = position
+        board.grid[row][col] = pawn
+      end
         
       it 'returns valid forward moves within board boundaries' do
-        moves = pawn.valid_moves(position, board)
+        moves = pawn.valid_moves(board)
         
-        expect(moves).to include([6, 2]) # normal move
-        expect(moves).to include([6, 3]) # special move
+        expect(moves).to include([5, 1]) # normal move
+        expect(moves).to include([4, 1]) # double move
       end
 
-      it 'does not include capture moves' do
-        moves = pawn.valid_moves(position, board)
+      it 'returns valid forward moves within board boundaries and not obstructed' do
+        board.grid[6][1] = Pawn.new(:black, [6, 1])
+        moves = pawn.valid_moves(board)
+        
+        expect(moves).to include([5, 1]) # normal move
+        expect(moves).to include([4, 1]) # double move
+      end
 
-        expect(moves).not_to include([5, 1])
-        expect(moves).not_to include([5, 3])
+      it 'returns valid one forward move when obstructed' do
+        board.grid[4][1] = Pawn.new(:white, [4, 1])
+
+        moves = pawn.valid_moves(board)
+
+        expect(moves).to include([5, 1])
+        expect(moves).to_not include([4, 1]) # obstructed by another pawn
+      end
+      
+      it 'returns no valid moves when obstructed' do
+        board.grid[5][1] = Pawn.new(:white, [5, 1])
+
+        moves = pawn.valid_moves(board)
+
+        expect(moves).to eq([])
+      end
+
+      it 'returns only one step move when already moved' do
+        board.grid[4][1] = Pawn.new(:white, [4, 1])
+        another_pawn = board.grid[4][1]
+
+        expect(another_pawn.valid_moves(board)).to include([3, 1])
       end
     end
 
     context 'when pawn color is black' do
       let(:board) { Board.new }
-      let(:position) { position = [1, 1] } # second top row
+      let(:position) { [1, 1] } # second top row
       subject(:pawn) { described_class.new(:black, position) }
 
-      it 'returns valid forward moves within board boundaries' do
-        moves = pawn.valid_moves(position, board)
+      before do
+        row, col = position
+        board.grid[row][col] = pawn
+      end
         
-        expect(moves).to include([1, 2]) # normal move
-        expect(moves).to include([1, 3]) # special move
+      it 'returns valid forward moves within board boundaries' do
+        moves = pawn.valid_moves(board)
+        
+        expect(moves).to include([2, 1]) # normal move
+        expect(moves).to include([3, 1]) # double move
       end
 
-      it 'does not include capture moves' do
-        moves = pawn.valid_moves(position, board)
+      it 'returns valid one forward move when obstructed' do
+        board.grid[3][1] = Pawn.new(:black, position)
 
-        expect(moves).not_to include([0, 2])
-        expect(moves).not_to include([2, 3])
+        moves = pawn.valid_moves(board)
+
+        expect(moves).to include([2, 1]) # normal
+        expect(moves).to_not include([3, 1]) # obstructed
+      end
+      
+      it 'returns no valid moves when obstructed' do
+        board.grid[2][1] = Pawn.new(:black, position)
+
+        moves = pawn.valid_moves(board)
+
+        expect(moves).to eq([])
+      end
+
+      it 'returns only one step move when already moved' do
+        board.grid[2][1] = Pawn.new(:black, [2, 1])
+        another_pawn = board.grid[2][1]
+
+        expect(another_pawn.valid_moves(board)).to include([3, 1])
       end
     end
   end
 
   describe '#valid_capture_moves' do
-    context 'when pawn is white, at [4, 3]' do
+    context 'when pawn is white' do
       let(:board) { Board.new }
       let(:position_white) { [4, 3] }
       subject(:pawn_white) { described_class.new(:white, position_white) }
       
-
       before do
-        board.grid[4][3] = pawn_white
+        row, col = position_white
+        board.grid[row][col] = pawn_white
       end
       
-      it 'shows additional move that signals capture when opposing pawn at [5, 4]' do
-        position_black = [5, 4] 
-        pawn_black = Pawn.new(:black, position_black)
-        board.grid[5][4] = pawn_black
-        moves = pawn_white.valid_capture_moves(position_white, board)
+      it 'returns a move that allows for capture' do
+        position_black = [3, 4]
+        row, col = position_black 
+        board.grid[row][col] = Pawn.new(:black, position_black)
+        moves = pawn_white.valid_capture_moves(board)
 
-        expect(moves).to include([5, 4])
+        expect(moves).to include([3, 4])
       end
 
-      it 'shows no capture moves when no pieces relative to pawn diagonally' do
-        moves = pawn_white.valid_capture_moves(position_white, board)
+      it 'returns two moves when there are two pieces diagonal against the pawn' do
+        position_black = [3, 2]
+        row, col = position_black 
+        board.grid[row][col] = Pawn.new(:black, position_black)
+        moves = pawn_white.valid_capture_moves(board)
+
+        expect(moves).to include([3, 2])
+      end
+
+      it 'returns no valid capture moves when opposing pawn is not diagonally placed' do
+        position_black = [3, 3]
+        row, col = position_black 
+        board.grid[row][col] = Pawn.new(:black, position_black)
+        moves = pawn_white.valid_capture_moves(board)
 
         expect(moves).to eq([])
+      end
+
+      it 'does not cannibalize same team' do
+        position_white = [3, 4] 
+        row, col = position_white
+        board.grid[row][col] = Pawn.new(:white, position_white)
+        moves = pawn_white.valid_capture_moves(board)
+
+        expect(moves).to_not include([3, 4])
       end
     end
     
-    context 'when pawn is black, at [5, 4] and opposing pawn at [4, 5]' do
+    context 'when pawn is black' do
       let(:board) { Board.new }
-      let(:position_black) { [5, 4] }
+      let(:position_black) { [3, 5] }
       subject(:pawn_black) { described_class.new(:black, position_black) }
       
       before do
-        board.grid[5][4] = pawn_black
+        row, col = position_black
+        board.grid[row][col] = pawn_black
       end
       
-      it 'shows additional move that signals capture when opposing pawn at [4, 5]' do
-        position_white = [4, 5] 
-        pawn_white = Pawn.new(:white, position_white)
-        board.grid[4][5] = pawn_white
-        moves = pawn_black.valid_capture_moves(position_black, board)
+      it 'shows a move that allows for capture' do
+        position_white = [4, 4] 
+        row, col = position_white 
+        board.grid[row][col] = Pawn.new(:white, position_white)
+        moves = pawn_black.valid_capture_moves(board)
 
-        expect(moves).to include([4, 5])
+        expect(moves).to include([4, 4])
       end
 
-      it 'shows no capture moves when no pieces relative to pawn diagonally' do
-        moves = pawn_black.valid_capture_moves(position_black, board)
+      it 'shows two moves when there are two pieces diagonal against the pawn' do
+        position_white = [4, 6] 
+        row, col = position_white
+        board.grid[row][col] = Pawn.new(:white, position_white)
+        moves = pawn_black.valid_capture_moves(board)
+
+        expect(moves).to include([4, 6])
+      end
+
+      it 'returns no valid capture moves when opposing pawn is not diagonally placed' do
+        position_white = [3, 3]
+        row, col = position_white 
+        board.grid[row][col] = Pawn.new(:white, position_white)
+        moves = pawn_black.valid_capture_moves(board)
 
         expect(moves).to eq([])
       end
-    end
-  end
 
-  describe '#moves' do
-    context 'when pawn is white and is at [6, 1]' do
-      let(:board) { Board.new }
-      let(:position) { position = [6, 1] } # second bottom row
-      subject(:pawn_test) { described_class.new(:white, position) }
-      let(:available_moves) { [[6, 3], [6, 2]] }
+      it 'does not cannibalize same team' do
+        position_black = [4, 6]
+        row, col = position_black 
+        board.grid[row][col] = Pawn.new(:black, position_black)
+        moves = pawn_black.valid_capture_moves(board)
 
-      before do
-        allow($stdout).to receive(:puts) # suppress output
-        allow(pawn_test).to receive(:available_moves).with(position, board).and_return(available_moves)
-        allow(pawn_test).to receive(:player_input).and_return(1) # simulate player choosing index 1
-      end
-
-      it 'updates @current_pos based on player input' do
-        pawn_test.moves(position, board)
-        expect(pawn_test.instance_variable_get(:@current_pos)).to eq([6, 2])
-      end
-    end
-
-    context 'when pawn is black and is at [1, 1]' do
-      let(:board) { Board.new }
-      let(:position) { position = [1, 1] } # second bottom row
-      subject(:pawn_test) { described_class.new(:black, position) }
-      let(:available_moves) { [[1, 3], [1, 2]] }
-
-      before do
-        allow($stdout).to receive(:puts) # suppress output
-        allow(pawn_test).to receive(:available_moves).with(position, board).and_return(available_moves)
-        allow(pawn_test).to receive(:player_input).and_return(0) # simulate player choosing index 0
-      end
-
-      it 'updates @current_pos based on player input' do
-        pawn_test.moves(position, board)
-        expect(pawn_test.instance_variable_get(:@current_pos)).to eq([1, 3])
+        expect(moves).to_not include([4, 6])
       end
     end
   end
