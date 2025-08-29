e4ac7bd83bd0a9e5c3e289b3a60b4507efacfd11
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com>
2025-08-29 07:39:41 +0700

    Adds array in initialization for storing captured pieces

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,11 @@
 class Player
   attr_reader :name, :color
+  attr_accessor :captured_pieces
 
   def initialize(name, color)
     @name = name
     @color = color
+    @captured_pieces = []
   end
 
   def to_s
