580f3f910b41755f4e9fe38d21baadf5cb4f3d1f
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com>
2025-09-03 19:59:41 +0700

    Adds method for enabling and returning valid en passant move

--- lib/piece/pawn.rb
+++ lib/piece/pawn.rb
@@ -9,13 +9,15 @@ class Pawn < Piece
   }
 
   attr_reader :color, :symbol, :default_position
-  attr_accessor :current_pos
+  attr_accessor :current_pos, :en_passant, :last_pos
 
   def initialize(color, position)
     super(color, position) # calls Piece#initialize
     @current_pos = position
     @symbol = color == :white ? "♙" : "♟"
     @default_position = color == :white ? [6, position[1]] : [1, position[1]]
+    @last_pos = []
+    @en_passant = false
   end
 
   def within_boundary?(row, col, board)
@@ -62,7 +64,8 @@ class Pawn < Piece
       next unless within_boundary?(new_x, new_y, board)
 
       target = board.grid[new_x][new_y]
-      if target && target.color != @color
+      condition = target && target.color != @color
+      if condition
         captures << [new_x, new_y]
       end
     end
@@ -70,13 +73,37 @@ class Pawn < Piece
   end
 
   def available_moves(board)
-    array = valid_moves(board) + valid_capture_moves(board)
+    array = valid_moves(board) + valid_capture_moves(board) + valid_enpassant_move(board)
     moves = {}
     i = 0
-
     array.each_with_index { |cell, i| moves[i] = cell }
-
     moves
+  end
+
+  def en_passant?(board)
+    start_row = (@color == :white) ? 6 : 1
+    x, y = @current_pos
+    last_x, last_y = @last_pos.last
+    
+    if last_x == start_row && (x - last_x).abs == 2
+      @en_passant = true
+    else
+      @en_passant = false
+    end
+  end
+
+  def valid_enpassant_move(board)
+    x, y = @current_pos
+    move = []
+
+    [-1, 1].each do |dy|
+      neighbor = board.grid[x][y + dy] rescue nil
+      if neighbor.is_a?(Pawn) && neighbor.en_passant
+        move << [x + (@color == :white ? -1 : 1), y + dy]
+      end
+    end
+
+    move
   end
 end
 
@@ -84,10 +111,54 @@ end
 
 # board = Board.new
 
-# position_white = [0, 1]
+# position_white = [6, 1]
+# white_pawn = Pawn.new(:white, position_white)
 # wx, wy = position_white
-# board.grid[wx][wy] = Pawn.new(:white, position_white)
-# white_pawn = board.grid[wx][wy]
+# board.grid[wx][wy] = white_pawn
+# white_pawn.last_pos << position_white
+
+# board.render
+# puts ""
+
+# white_pawn.last_pos << position_white
+# board.grid[wx][wy] = nil
+# position_white = [4, 1]
+# wx, wy = position_white
+# board.grid[wx][wy] = white_pawn
+# white_pawn.current_pos = position_white
+
+# board.render
+# puts ""
+
+# board.grid[wx][wy] = nil
+# position_white = [3, 1]
+# wx, wy = position_white
+# board.grid[wx][wy] = white_pawn
+# white_pawn.current_pos = position_white
+
+# board.render
+# puts ""
+
+# position_black = [1, 2]
+# black_pawn = Pawn.new(:black, position_black)
+# bx, by = position_black
+# board.grid[bx][by] = black_pawn
+
+# board.render
+# puts ""
+
+# black_pawn.last_pos << position_black
+# board.grid[bx][by] = nil
+# position_black = [3, 2]
+# bx, by = position_black
+# board.grid[bx][by] = black_pawn
+# black_pawn.current_pos = position_black
+
+# board.render
+# # stubs black pawn
+# p black_pawn.en_passant?(board) # true
+# p white_pawn.available_moves(board)
+# puts ""
 
 # board.render
 # p white_pawn.valid_moves(board)
