4e7bee5f1fe8cd02e3d3d053c6ef965db4d5fece
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com>
2025-09-05 15:44:06 +0700

    changes the render to include coordination

--- lib/board.rb
+++ lib/board.rb
@@ -82,11 +82,21 @@ class Board
   end
 
   def render
-    @grid.each do |row|
+    (1..8).to_a.reverse.each_with_index do |letter, i|
+      break if i >= @grid.size
+      print "#{letter} "
+      row = @grid[i]
       puts row.map { |cell| cell.nil? ? "." : cell.symbol }.join(" ")
     end
+
+    print "  "
+    ('a'..'h').each { |letter| print "#{letter} "}
+    puts
   end
 end
+
+# board = Board.new
+# board.render
 
 # require_relative 'player'
 
