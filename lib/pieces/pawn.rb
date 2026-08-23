0d27aea11177211960dcafa169f72304fe13426d
GroupBAudi <144666972+GroupBAudi@users.noreply.github.com>
2026-08-23 21:27:35 +0700

    add method to expire en passant using polymorphism

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -109,8 +109,12 @@ class Pawn < Piece
     move
   end
 
-    def after_move(board)
+  def after_move(board)
     # do stuff after something move i.e. pawn move for en passant, king move after checked expires castling
     en_passant?(board)
+  end
+
+  def expire_move_state
+    self.en_passant = false
   end
 end
